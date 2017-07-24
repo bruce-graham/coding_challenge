f8b196756d6002a319bbe495090476910a34467d
Bruce <brucegraham11@gmail.com>
2017-07-24 15:31:41 -0700

    Refactor of http.request that fetches the html of specified website.

--- server/worker.js
+++ server/worker.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var http = require('http');
+var util = require('util');
 
 var options = {
-  host: "",
+  host: '',
   port: 80,
   path: '/index.html'
 };
@@ -12,23 +13,35 @@ var options = {
 setInterval(function() {
   request('http://127.0.0.1:8888/worker', function (error, response, body) {
     if (error) {
-      console.log(error);
+      console.log('line 14 worker.js error', error);
     } else {
-      console.log("it connected", body);
       if (body !== 'Nothing to dequeue') {
         options.host = body;
 
-        http.get(options, function(response) {
-          console.log(response.body);
-        }).on('error', function (errror) {
-          console.log(error);
+        var content = '';
+
+        var request = http.request(options, function(response) {
+            response.setEncoding("utf8");
+            response.on('data', function (chunk) {
+              content += chunk;
+            });
+
+            response.on('end', function () {
+              util.log('this is content :', content);
+            });
+
+            response.on('error', function (error) {
+              console.log(error);
+            });
         });
+
+        request.end();
+      } else {
+        console.log("Worker checked queue and nothing was there");
       }
     }
-  })
-
-  }, 10000
-);
+  });
+}, 5000);
 
 app.listen('8000', function() {
   console.log("Listening on port 8000");
