9695d146472ee8cbe6702130d5410d2fe4bfc157
Bruce <brucegraham11@gmail.com>
2017-07-27 00:45:35 -0700

    Add the foundation of the Sequelize authentication and schema

---
+++ database/database.js
@@ -0,0 +1,35 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('massdrop', 'root', 'javascript', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate()
+  .then(function() {
+    console.log('Connection has been established to database.');
+  })
+  .catch(function(err) {
+    console.error('Unable to connect to the database: ', err);
+  });
+
+var Site = sequelize.define('Site', {
+  uniqueId: {
+    type: Sequelize.STRING
+  },
+  url: {
+    type: Sequelize.STRING
+  },
+  html: {
+    type: Sequelize.TEXT
+  }
+});
+
+
+module.exports = Site;
