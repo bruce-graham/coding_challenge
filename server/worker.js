86954d7410cd67fa5324c8e8cb3804a2931bb5c7
Bruce <brucegraham11@gmail.com>
2017-07-27 00:46:41 -0700

    Add database/Sequelize sync, create and findAll

--- server/worker.js
+++ server/worker.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var axios = require('axios');
+var Site = require('../database/database.js');
+// var queue = require('../server/server.js');
 
 
 setInterval(function() {
@@ -16,10 +18,22 @@ setInterval(function() {
 
         axios.get(url)
           .then(function(response) {
-            console.log('response.data => ', response.data);
+            console.log('We got HTML from the specified url => response.data');
+
+            Site.sync({force: true}).then(function() {
+              return Site.create({
+                uniqueId: '1',
+                url: 'www.google.com',
+                html: '<!DOCTYPE html><html><head></head><body></body></html>'
+              });
+            });
+
+            Site.findAll().then(function(sites) {
+              console.log('this is the info in the database ', sites);
+            })
           })
           .catch(function(error) {
-            console.log('error on worker.js line 24', error);
+            console.log('error on worker.js ', error);
           });
       } else {
         console.log("Worker checked queue and nothing was there");
