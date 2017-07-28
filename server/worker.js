bcf4695ad3e37ea65a44e0c3a6902f8158480178
Bruce <brucegraham11@gmail.com>
2017-07-27 23:01:25 -0700

    Replace dummy data with dynamic data that is stored to the database.

--- server/worker.js
+++ server/worker.js
@@ -3,34 +3,33 @@ var app = express();
 var request = require('request');
 var axios = require('axios');
 var Site = require('../database/database.js');
-// var queue = require('../server/server.js');
 
 
 setInterval(function() {
   request('http://127.0.0.1:8888/api/worker', function (error, response, body) {
-
     if (error) {
       console.log('line 11 worker.js error', error);
     } else {
       if (body !== 'Nothing to dequeue') {
-        var url = 'http://';
-        url = url + body
+        var bodyObj = JSON.parse(body);
+        var website = 'http://';
+        website += bodyObj.url;
 
-        axios.get(url)
+        axios.get(website)
           .then(function(response) {
-            console.log('We got HTML from the specified url => response.data');
-
             Site.sync({force: true}).then(function() {
+              var uniqueId = bodyObj.uniqueId;
+              var url = bodyObj.url
+              var html = response.data;
+
+              console.log('site.create =>', uniqueId, url, html);
+
               return Site.create({
-                uniqueId: '1',
-                url: 'www.google.com',
-                html: '<!DOCTYPE html><html><head></head><body></body></html>'
+                uniqueId: uniqueId,
+                url: url,
+                html: html
               });
             });
-
-            Site.findAll().then(function(sites) {
-              console.log('this is the info in the database ', sites);
-            })
           })
           .catch(function(error) {
             console.log('error on worker.js ', error);
