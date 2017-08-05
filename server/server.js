c1240ec8458139add03bfe4f01b03ac420a0a71f
Bruce Graham <brucegraham11@gmail.com>
2017-08-04 22:30:04 -0700

    Remove queue data structure

--- server/server.js
+++ server/server.js
@@ -1,39 +1,7 @@
 var express = require('express');
 var app = express();
 var Site = require('../database/database.js');
-var tempId = 0;
-
-var Queue = function() {
-  this.storage = {};
-  this.currentNumber = 0;
-  this.totalInQueue = 0;
-};
-
-Queue.prototype.enqueue = function(value) {
-  this.currentNumber++;
-  this.totalInQueue++;
-  this.storage[this.currentNumber] = {value: value, id: tempId};
-};
-
-Queue.prototype.dequeue = function() {
-  var output = {};
-
-  if (this.totalInQueue > 0) {
-    this.totalInQueue--;
-    var temp = this.storage[this.currentNumber - this.totalInQueue]['value'];
-    delete this.storage[this.currentNumber - this.totalInQueue];
-    output.uniqueId = tempId;
-    output.url = temp;
-    return output;
-  }
-  return 'Nothing to dequeue';
-};
-
-Queue.prototype.size = function() {
-  return this.totalInQueue;
-};
-
-var queue = new Queue();
+var queue = require('../server/queue.js');
 
 app.get('/api/sites/:url', function(req, res) {
   var websiteUrl = req.params.url;
@@ -45,25 +13,31 @@ app.get('/api/sites/:url', function(req, res) {
     });
   })
   .then(function (data) {
+    console.log('first console of queue in server.js', queue);
+
     var uniqueId = data.dataValues.id.toString();
-    tempId = uniqueId;
+    queue.tempId = uniqueId;
     queue.enqueue(websiteUrl);
+
+    console.log('second console of queue in server.js', queue);
+
     res.send(uniqueId);
-  })
+  });
 });
 
 app.get('/api/worker', function(req, res) {
   var value = queue.dequeue();
+  console.log('server.js /api/worker  value =>', value);
   res.send(value);
 });
 
 app.get('/api/jobs/:id', function(req, res) {
   var uniqueId = req.params.id;
 
-  Site.findOne({where:{id: uniqueId}})
+  Site.findOne({where:{id: queue.uniqueId}})
     .then(function(data) {
       var output = {}
-      output.id = uniqueId;
+      output.id = queue.uniqueId;
 
       if (data === null) {
         res.send('Not a valid ID, please submit another ID.');
@@ -76,9 +50,8 @@ app.get('/api/jobs/:id', function(req, res) {
         output.html = decodeURIComponent(data.html);
         res.send(output);
       }
-    })
+    });
 });
-
 
 app.listen(8888, function () {
   console.log("Listening on port 8888");
